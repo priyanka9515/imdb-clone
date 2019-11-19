ce46435befef4508def58bea3f9ed5769d7b95e7
madhu-kuncham <kmadhukumar88011@gmail.com>
2019-11-19 20:35:07 +0530

    changes in moviecontainer

--- src/components/home/MoviesContainer.js
+++ src/components/home/MoviesContainer.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class MoviesContainer extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
-export default MoviesContainer
+export default MoviesContainer;
